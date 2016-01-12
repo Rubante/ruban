adf8dff8b00f3447f03c433c38a04d65d320ee73
rubant@163.com <rubant@163.com>
2016-01-12 11:25:05 +0800

    测试功能添加

--- framework/dao/src/test/java/com/ruban/framework/dao/database/DataSourceConfiguration4Test.java
+++ framework/dao/src/test/java/com/ruban/framework/dao/database/DataSourceConfiguration4Test.java
@@ -1,8 +1,14 @@
 package com.ruban.framework.dao.database;
 
-import org.junit.BeforeClass;
+import java.sql.Connection;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.excel.XlsDataSet;
+import org.dbunit.operation.DatabaseOperation;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.stereotype.Component;
@@ -12,8 +18,18 @@ public class DataSourceConfiguration4Test implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        new EmbeddedDatabaseBuilder().setName("rubanDao").setType(EmbeddedDatabaseType.H2).addScript("classpath:H2_TYPE.sql")
-                .addScript("classpath:INIT_TABLE.sql").build();
+
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        builder.setName("rubanDao");
+        builder.setType(EmbeddedDatabaseType.H2);
+        builder.addScript("classpath:H2_TYPE.sql");
+        builder.addScript("classpath:INIT_TABLE.sql");
+        Connection connection = builder.build().getConnection();
+        IDataSet dataSet = new XlsDataSet(new ClassPathResource("data.xls").getFile());
+
+        IDatabaseConnection databaseConnection = new DatabaseConnection(connection);
+        DatabaseOperation databaseOperation = ((DatabaseOperation) DatabaseOperation.CLEAN_INSERT);
+        databaseOperation.execute(databaseConnection, dataSet);
 
     }
 }
