26ce229bb8ebfce6882fb5092ebf5652a9a2fa6f
rubant <rubant@163.com>
2017-03-14 15:16:48 +0800

    一次性提交

--- module/rbac/src/main/java/com/ruban/rbac/dao/ICompanyMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ruban.rbac.dao;
-
-import com.ruban.framework.dao.IRubanMapper;
-import com.ruban.framework.dao.support.RubanDaoRepository;
-
-@RubanDaoRepository
-public interface ICompanyMapper extends IRubanMapper {
-}
