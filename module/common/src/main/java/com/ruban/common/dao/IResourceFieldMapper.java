3fec9ef359ee5a482e2f15bbd01fb24e79e20d8b
rubant <rubant@163.com>
2017-06-01 11:33:43 +0800

    大模块变动

--- module/common/src/main/java/com/ruban/common/dao/IResourceFieldMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ruban.common.dao;
-
-import com.ruban.framework.dao.IRubanMapper;
-import com.ruban.framework.dao.support.RubanDaoRepository;
-
-@RubanDaoRepository
-public interface IResourceFieldMapper extends IRubanMapper {
-}
--- module/rbac/src/main/java/com/ruban/rbac/dao/authz/IPermissionResourceMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ruban.rbac.dao.authz;
-
-import com.ruban.framework.dao.IRubanMapper;
-import com.ruban.framework.dao.support.RubanDaoRepository;
-
-@RubanDaoRepository
-public interface IPermissionResourceMapper extends IRubanMapper {
-}
